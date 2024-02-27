d1d7f5d1f6db3377023003096319755e23bcd616
Wnzxy <104203492+Wnzxy@users.noreply.github.com>
2024-02-27 08:06:59 +0100

    Fixed nodemon

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Hello Worrld")
 })
 app.listen(4000, () => {
     console.log("Running on 4k port")
